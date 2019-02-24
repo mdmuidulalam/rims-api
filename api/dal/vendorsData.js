9c23258c773f8fb458c7510d27a88271a451a845
Jahid Hasan <jsajeeb09@gmail.com>
2019-02-24 23:57:02 +0600

    F-10: Made basic api for vendor

---
+++ api/dal/vendorsData.js
@@ -0,0 +1,44 @@
+var baseData = require("./baseData");
+var dynamicModel = require("../models/dynamicModel");
+
+class vendorsData extends baseData {
+  constructor(dbConnection, vendorsColumns) {
+    super(dbConnection);
+    this.vendors = new dynamicModel(dbConnection).getModel(
+      "Vendors",
+      vendorsColumns
+    );
+  }
+
+  /* Inset data in vendors table */
+
+  /// Insert vendor in vendors table
+  insertVendor(vendor) {
+    return this.vendors.create(vendor);
+  }
+
+  /* End inset data in vendors table */
+
+  /* Update data in vendors table */
+
+  /// Update vendor in vendors table
+  updateVendor(vendor) {
+    let id = vendor["Id"];
+    delete vendor["Id"];
+    return this.vendors.update(vendor, { where: { Id: { $eq: id } } });
+  }
+
+  /* End update data in vendors table */
+
+  /* Get data in vendors table */
+
+  /// Get vendor in vendors table
+  getVendors(query) {
+    query = this.getSequelizeQuery(query);
+    return this.vendors.findAll({ where: query });
+  }
+
+  /* End get data in vendors table */
+}
+
+module.exports = vendorsData;
